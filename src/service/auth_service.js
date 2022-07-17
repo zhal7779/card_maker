74ca088961a6173db156fb817b5b916d05ce2fd4
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2022-07-18 04:07:49 +0900

    add login component

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,8 +1,12 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import firebaseApp from './firebase';
+
+
 class AuthService {
     login(providerName) {
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-        return firebase.auth().signInWithPopup(authProvider);
+        return firebaseApp.auth().signInWithPopup(authProvider);
     } 
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase/compat/app';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,4 +9,5 @@ const firebaseConfig = {
     };
 
     // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+    export default firebaseApp;
