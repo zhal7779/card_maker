2b97428248a2c02b801b4514c8985ee3f9a5acce
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2022-07-24 17:15:03 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -6,6 +6,42 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from'./maker.module.css';
 const Maker = ({authService}) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'minyoung1',
+            company: 'Naver',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'zhal7779@naver.com',
+            message: 'go for it',
+            fileName:'minyoung',
+            fileURL: null,
+        },
+        {
+            id: '2',
+            name: 'minyoung2',
+            company: 'Naver',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'zhal7779@naver.com',
+            message: 'go for it',
+            fileName:'minyoung',
+            fileURL: null,
+        },
+        {
+            id: '3',
+            name: 'minyoung3',
+            company: 'Naver',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'zhal7779@naver.com',
+            message: 'go for it',
+            fileName:'minyoung',
+            fileURL: null,
+        }
+    ]);
+
     const navigate = useNavigate();
 
     const onLogout = () =>{
@@ -24,8 +60,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards ={cards}/>
+                <Preview cards ={cards}/>
             </div>
             <Footer />
 
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './preview.module.css';
+import Card from '../card/card';
 
-const Preview = (props) =>(
+const Preview = ({cards}) =>(
     <section className={styles.preview}>
         <h1 className={styles.title}>Card Preview</h1>
+        <ul className={styles.cards}>
+            {cards.map(card =>(
+                <Card card={card}></Card>
+            ))}
+        </ul>
     </section>
 );
 
